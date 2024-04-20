d159dbfb90f24a605552c9e44803e33ca09a10a7
chris-plonka <info@krzysztofplonka.pl>
2024-04-20 17:47:54 +0200

    Finished pet filtering

--- main.js
+++ main.js
@@ -14,14 +14,15 @@ start()
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
-  petsData.forEach((pet) => {
+  petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
 
+    clone.querySelector(".pet-card").dataset.species = pet.species
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
 
-    if (!pet.photo) pet.photo = "/images/fallback.jpg"
+    if (!pet.photo) pet.photo = "images/fallback.jpg"
 
     clone.querySelector(".pet-card-photo img").src = pet.photo
     clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
@@ -46,17 +47,28 @@ function createAgeText(birthYear) {
 // pet filter button code
 const allButtons = document.querySelectorAll(".pet-filter button")
 
-document.querySelectorAll(".pet-filter button").forEach((el) => {
+allButtons.forEach(el => {
   el.addEventListener("click", handleButtonClick)
 })
 
 function handleButtonClick(e) {
   // remove active class from any and all buttons
 
-  allButtons.forEach((el) => el.classList.remove("active"))
+  allButtons.forEach(el => el.classList.remove("active"))
 
-  //add active class to specific button that just got clicked
+  //add active class to the specific button that just got clicked
   e.target.classList.add("active")
 
   // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+
+      el.style.display = "grid"
+    }
+   else {
+    el.style.display = "none"
+   }
+
+  })
 }
